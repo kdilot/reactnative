80df6b0b2775330704c0782e8962fcfe74f5a2ed
kdilot <kdilotdev@gmail.com>
2019-11-29 14:41:03 +0900

    files

--- App.js
+++ App.js
@@ -1,19 +1,94 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createMaterialTopTabNavigator } from "react-navigation-tabs";
+import { createStackNavigator } from "react-navigation-stack";
+import FirstPage from "./components/navigation/FirstPage";
+import SecondPage from "./components/navigation/SecondPage";
+import { Button } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const TabScreen = createMaterialTopTabNavigator(
+  {
+    Home: {
+      screen: FirstPage,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="md-home" size={25} color="white" />
+        )
+      }
+    },
+    Settings: {
+      screen: SecondPage,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="md-alert" size={25} color="white" />
+        )
+      }
+    }
   },
+  {
+    tabBarPosition: "top",
+    swipeEnabled: true,
+    animationEnabled: true,
+    tabBarOptions: {
+      showIcon: true,
+      showLabel: false,
+      activeTintColor: "red",
+      inactiveTintColor: "#F8F8F8",
+      style: {
+        backgroundColor: "#633689"
+      },
+      labelStyle: {
+        textAlign: "center"
+      },
+      indicatorStyle: {
+        borderBottomColor: "red",
+        borderBottomWidth: 2
+      }
+    }
+  }
+);
+
+TabScreen.navigationOptions = ({ navigation, screenProps }) => ({
+  headerStyle: {
+    backgroundColor: "#633689"
+  },
+  headerTintColor: "#FFFFFF",
+  title: "TabExample",
+  headerLeft: (
+    <Ionicons
+      name="md-menu"
+      size={32}
+      color={"white"}
+      style={{ marginLeft: 10 }}
+    />
+    // <Button
+    //   title="Menu"
+    //   onPress={navigation => {
+    //     navigation.navigate("DrawerOpen");
+    //   }}
+    // />
+  ),
+  headerRight: (
+    <Ionicons
+      name="md-search"
+      size={32}
+      color={"white"}
+      style={{ marginRight: 10 }}
+    />
+    // <Button
+    //   title="Menu"
+    //   onPress={navigation => {
+    //     navigation.navigate("DrawerOpen");
+    //   }}
+    // />
+  )
 });
+
+//making a StackNavigator to export as default
+const App = createStackNavigator({
+  TabScreen: {
+    screen: TabScreen
+  }
+});
+export default createAppContainer(App);
