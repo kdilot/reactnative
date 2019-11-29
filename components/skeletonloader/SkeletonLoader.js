ab6ed5e29effb7a839acfe042b5bcba89f529e3a
kdilot <kdilotdev@gmail.com>
2019-11-29 13:05:22 +0900

    files

---
+++ components/skeletonloader/SkeletonLoader.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  Placeholder,
+  PlaceholderMedia,
+  PlaceholderLine,
+  Fade,
+  Shine,
+  Loader,
+  Progressive,
+  ShineOverlay
+} from "rn-placeholder";
+import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+const width = Dimensions.get("window").width;
+
+const ShapeRect = ({ ani = Fade }) => {
+  return (
+    <View style={Styles.box}>
+      <Placeholder Animation={ani} Left={PlaceholderMedia}>
+        <PlaceholderLine />
+        <PlaceholderLine />
+        <PlaceholderLine />
+      </Placeholder>
+    </View>
+  );
+};
+
+const SkeletonLoader = () => {
+  return (
+    <ScrollView>
+      <View style={Styles.container}>
+        <ShapeRect ani={Fade} />
+        <ShapeRect ani={Shine} />
+        <ShapeRect ani={Loader} />
+        <ShapeRect ani={Progressive} />
+        <ShapeRect ani={ShineOverlay} />
+        <ShapeRect ani={ShineOverlay} />
+        <ShapeRect ani={ShineOverlay} />
+        <ShapeRect ani={ShineOverlay} />
+        <ShapeRect ani={ShineOverlay} />
+        <ShapeRect ani={ShineOverlay} />
+        <ShapeRect ani={ShineOverlay} />
+      </View>
+    </ScrollView>
+  );
+};
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width,
+    padding: 30
+  },
+  box: {
+    flex: 1,
+    marginTop: 10,
+    marginBottom: 10
+  }
+});
+
+export default SkeletonLoader;
